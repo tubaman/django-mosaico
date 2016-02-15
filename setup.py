2a6157c2c9d1616a020403934da9a7d2ee710912
Ryan Nowakowski <tubaman@fattuba.com>
2016-02-15 15:19:39 -0600

    use find_packages in setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 import os
-from setuptools import setup
+from setuptools import setup, find_packages
 
 with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
     README = readme.read()
@@ -10,7 +10,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 setup(
     name='django-mosaico',
     version='0.1',
-    packages=['mosaico', 'mosaico.migrations'],
+    packages=find_packages('.'),
     include_package_data=True,
     license='BSD License',  # example license
     description='A django app that contains the mosaico frontend and implements the mosaico backend.',
